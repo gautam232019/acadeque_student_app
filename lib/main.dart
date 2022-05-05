b3b75ff5d0799cd63746342e5bf3891b05898f60
DemonKING073 <legendsnever7die@gmail.com>
2022-05-05 19:44:57 +0545

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -24,12 +24,15 @@ import 'package:acadeque_student_app/modules/tutoring_screen/tutoring_screen.dar
 import 'package:acadeque_student_app/modules/tutoring_screen/tutoring_state.dart';
 import 'package:acadeque_student_app/modules/welcome_screen/welcome_screen.dart';
 import 'package:acadeque_student_app/modules/welcome_screen/welcome_state.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:oktoast/oktoast.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await GetStorage.init();
   runApp(const App());
 }
